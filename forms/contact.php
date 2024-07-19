829410168c96916a2f04019758f4a290d10787b2
Rocky patel <120505241+Rpatel9675@users.noreply.github.com>
2024-07-19 23:13:57 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,27 +1,38 @@
 <?php
 
-  $receiving_email_address = 'pprl735757@gmail.com';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$receiving_email_address = 'pprl735757@gmail.com';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+}
 
+if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include($php_email_form);
+} else {
+    die('Unable to load the "PHP Email Form" Library!');
+}
 
-  
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'];
+$contact->from_email = $_POST['email'];
+$contact->subject = $_POST['subject'];
 
-  echo $contact->send();
+$contact->add_message($_POST['name'], 'From');
+$contact->add_message($_POST['email'], 'Email');
+$contact->add_message($_POST['message'], 'Message', 10);
+
+if($contact->send()) {
+    echo 'Email sent successfully.';
+} else {
+    echo 'Failed to send email.';
+}
 ?>
